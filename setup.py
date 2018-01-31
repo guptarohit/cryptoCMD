7cff987205fe81783f93d9862f7fbd047e6f89e3
Rohit Gupta <rohitg.tech@gmail.com>
2018-01-31 23:23:49 +0530

    v0.2.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = '\n' + f.read()
 
 NAME = 'cryptocmd'
-VERSION = '0.2.0'
+VERSION = 'v0.2.1'
 
 with open('requirements.txt') as f:
     required = f.readlines()
