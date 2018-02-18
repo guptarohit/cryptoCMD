643880f074669f8143e30f1c7c3a72fdf71b5919
Rohit Gupta <rohitg.tech@gmail.com>
2018-02-18 19:46:30 +0530

    Add examples

---
+++ examples/get_csv.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from cryptocmd import CmcScraper
+
+# Initialise scraper with 'coin code' of cryptocurrency
+# If time interval is not passed all time data will be scrapped
+scraper = CmcScraper('btc')
+
+# You can pass name for the csv explicitly,
+# Else it will be named in format {coin_code}_{start_date}_{end_date}.csv
+scraper.export_csv('btc_all_time.csv')
