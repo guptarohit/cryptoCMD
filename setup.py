44fa6e5df1dba2aee1c6e945de73041dd558cd73
Rohit Gupta <guptarohit@users.noreply.github.com>
2018-02-23 23:02:40 +0530

    Update setup.py
    Add upload command
    Format setup.py

--- setup.py
+++ setup.py
@@ -1,35 +1,69 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import io
 import os
 import sys
-from codecs import open
-from setuptools import setup
+from shutil import rmtree
+from setuptools import setup, Command
+
+# Package meta-data.
+NAME = 'cryptocmd'
+DESCRIPTION = 'Cryptocurrency historical market price data scrapper.'
+URL = 'https://github.com/guptarohit/cryptocmd'
+AUTHOR = 'Rohit Gupta'
+VERSION = 'v0.3.2'
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+with io.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = '\n' + f.read()
 
-NAME = 'cryptocmd'
-VERSION = 'v0.3.1'
-
-with open('requirements.txt') as f:
+with io.open('requirements.txt') as f:
     required = f.readlines()
 
-if sys.argv[-1] == "publish":
-    os.system("python setup.py sdist bdist_wheel upload")
-    sys.exit()
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds...')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution...')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPi via Twine...')
+        os.system('twine upload dist/*')
+
+        sys.exit()
+
 
 setup(
     name=NAME,
     version=VERSION,
-    description='Cryptocurrency historical market price data scrapper.',
+    description=DESCRIPTION,
     long_description=long_description,
+    author=AUTHOR,
+    url=URL,
     packages=[NAME],
-    url='https://github.com/guptarohit/cryptocmd',
-    author='Rohit Gupta',
-    author_email='',
     install_requires=required,
     include_package_data=True,
     extras_require={
@@ -46,4 +80,8 @@ setup(
         'Programming Language :: Python :: 3.6',
         "License :: OSI Approved :: BSD License",
     ],
+    # $ setup.py publish support.
+    cmdclass={
+        'upload': UploadCommand,
+    },
 )
