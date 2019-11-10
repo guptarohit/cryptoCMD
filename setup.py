7f6f490939db53c23d18523937f463ebf9e7de45
Rohit Gupta <rohitg.tech@gmail.com>
2019-11-10 13:31:53 +0530

    add python 3.8

--- setup.py
+++ setup.py
@@ -77,6 +77,7 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: BSD License",
     ],
     # $ setup.py publish support.
