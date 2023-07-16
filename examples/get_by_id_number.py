9fb0661d7cca75b7f248f40bfe3ba534b0952f6f
Thales Tozatto <ttozatto@gmail.com>
2023-07-16 18:48:29 -0300

    id_number parameter added

---
+++ examples/get_by_id_number.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from cryptocmd import CmcScraper
+
+# Initialise scraper with 'id_number' of cryptocurrency.
+# 'id_number' is unique for each cryptocurrency, 
+#   making it safer to retrieve data from tokens with duplicate names.
+# 'id_number will override 'coin_code' and 'coin_name' when provided,
+#   a WARNING will be printed if 'id_number' is provided 
+#   and 'coin_code' or 'coin_name' provided are different from the coin returned.
+# If time interval is not passed all time data will be scrapped
+# 
+scraper = CmcScraper(coin_code='btc', coin_name='bitcoin', id_number=25931)
+
+# You can pass name for the csv explicitly,
+# Else it will be named in format {coin_code}_{start_date}_{end_date}.csv
+scraper.export("csv", name="TIME_all_time")
