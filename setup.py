61e2ad273994432216e7d0d8c9361613f53edde5
Rohit Gupta <rohitgtech+git@gmail.com>
2024-04-07 16:38:25 +0530

    Remove support for python 3.7
    as it reached eol

--- setup.py
+++ setup.py
@@ -73,11 +73,11 @@ setup(
         "Topic :: Utilities",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: BSD License",
     ],
     # $ setup.py publish support.
