1bdb08e4ca0c75739bc11cc4f91b0ce3b3d2c7dd
Rohit Gupta <rohitgtech+git@gmail.com>
2026-04-26 03:21:20 +0530

    Drop Python 3.8 support (EOL since Oct 2024)

--- setup.py
+++ setup.py
@@ -73,7 +73,6 @@ setup(
         "Topic :: Utilities",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
